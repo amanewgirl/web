8fa6d01185b401fd7e27135db1ccffef7195324d
Simon Wells <siwells@gmail.com>
2017-07-04 18:49:50 +0100

    Fix typp

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,5 +2,5 @@
 
 cd /home/40008609/arg@enu/arg.napier.ac.uk
 git pull
-cd /var/www$ 
+cd /var/www
 sudo rsync -avz --human-readable --progress /home/40008609/arg@enu/arg.napier.ac.uk/public/ arg.napier.ac.uk/
